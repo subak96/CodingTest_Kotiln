c9a982c1fd1da0d0307931208599e4e93f7ad239
준수 <139088858+subak96@users.noreply.github.com>
2023-12-07 15:59:02 +0900

    Update 나머지가 1이 되는 수 찾기.kt

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.kt
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.kt
@@ -1,6 +1,8 @@
 class Solution {
     fun solution(n: Int): Int {
+        //i는 2부터 n-1까지의 값을 가짐 n이 100이면 2부터99까지
         for(i in 2..n-1){
+            //n%i 를 해서 나머지가 1 이 나오면 바로 리턴하여 값 출력, 100 % 2부터 99까지를 나누고 그 중 가장 먼저 1이 되는 i를 찾아 출력
         if(n%i == 1) return i
         }
         return 0

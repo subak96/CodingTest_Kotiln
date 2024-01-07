c9307515614caa7dfd82b1e900c102d37214ec33
준수 <139088858+subak96@users.noreply.github.com>
2024-01-07 18:19:27 +0900

    [level unrated] Title: 홀짝 구분하기, Time: 183.08 ms, Memory: 45.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val n = readLine()!!.toInt()
+    
+    if(n % 2 == 0) {
+        println("$n is even")
+    }
+    else {
+        println("$n is odd")
+    }
+}
